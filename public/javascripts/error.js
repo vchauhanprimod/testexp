b53891576ffa31bd3fb374ef2d78586d75501371
varsha <nrat@nrat-Aspire-3820.(none)>
2014-03-28 19:54:01 +0530

    updated error list

--- public/javascripts/error.js
+++ public/javascripts/error.js
@@ -1,5 +1,22 @@
 exports.errmap = {
-    "ER_NO_SUCH_TABLE": "203 Non-Authoritative Information",
-    "ER_PARSE_ERROR": "400 Bad Request"
+    "ER_NO_SUCH_TABLE": "Error: 1146 NO such table exists",
+    "ER_PARSE_ERROR": "Error: 1064 Syntax Error",
+    "ER_EMPTY_QUERY":" Error: 1065 Query was empty",
+    "ER_NONUNIQ_TABLE":" Error: 1066 Not unique table",
+    "ER_CANT_REMOVE_ALL_FIELDS": " Error: 1090 You can't delete all columns with ALTER TABLE; use DROP TABLE instead",
+    "ER_CANT_CREATE_FILE": " Error: 1004 Can't create file",
+    "ER_CANT_CREATE_TABLE": "Error: 1005  Can't create table ",
+    "ER_CANT_CREATE_DB":"Error: 1006 Can't create database",
+    "ER_DB_CREATE_EXISTS":" Error: 1007 database exists",
+    "ER_DB_DROP_EXISTS":" Error: 1008  Can't drop database,database doesn't exist",
+    "ER_DB_DROP_DELETE":" Error: 1009 Error dropping database",
+    "ER_CANT_DELETE_FILE":" Error: 1011 Error on delete",
+    "ER_CANT_FIND_SYSTEM_REC":" Error: 1012  Can't read record in system table",
+    "ER_CANT_LOCK":" Error: 1015 Can't lock file",
+    "ER_CANT_OPEN_FILE":" Error: 1016 Can't open file",
+    "ER_FILE_NOT_FOUND":"Error: 1017 Can't find file",
+    "ER_CANT_READ_DIR":"Error: 1018  Can't read dir",
+    "ER_XA_RBDEADLOCK":" Error: 1479 Transaction branch was rolled back: deadlock was detected",
+    "ER_TOO_MANY_CONCURRENT_TRXS":" Error: 1480 Too many active concurrent transactions"   
 }
 
